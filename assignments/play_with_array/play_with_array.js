1517ac1063c499d44654bc54d543f717e040af19
madhan1411 <66825748+madhan1411@users.noreply.github.com>
2022-03-07 10:58:55 +0530

    play with array submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,14 @@ function getEven(arr) {
 
         Write you code below
     */
+   let arr1=[]
+   for(let i=0;i<arr.length;i++){
+       if(arr[i]%2===0){
+           arr1.push(arr[i])
+       }
 
+   }
+   return arr1
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +25,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    for(let i=0;i<arr.length;i++)
+    {
+        arr[i]=arr[i]*n
+    }
+    return arr
+
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +41,9 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+      arr.splice(n,1)
+        
+      return arr
 }
 
 module.exports = {
